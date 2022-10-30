3dff0feaf4050602107973c274396a80bd3f892c
“Aleksandar <radonic@live.dk>
2022-10-30 16:03:46 +0100

    Added retry to api call

--- lib/api_call.py
+++ lib/api_call.py
@@ -1,14 +1,26 @@
 from requests import get
-import json
+import time
 
 def call_api(url):
-    response = get(url=url)
-    status_code = response.status_code
-    data = response.text
+    #Count failed calls 
+    failed_api_calls = 0
+    
+    while failed_api_calls < 4:
+        #Call api
+        response = get(url=url)
+        #Retrieve status code and response data
+        status_code = response.status_code
+        data = response.text
 
-    if status_code != 200:
-        raise RuntimeError('Api call to url', url, 'failed with status code:', status_code, 'and message:', data)
+        #Return not found aka no more Pokemons to harvest
+        if status_code == 404:
+            return status_code, data
+        #Retry api call on failure
+        elif status_code != 200 and failed_api_calls < 4:
+            time.sleep(10)
+            failed_api_calls += 1
+            continue
 
-    data = json.loads(data)
+        return status_code, data
 
-    return status_code, data
+    raise RuntimeError('Api call to url', url, 'failed with status code:', status_code, 'and message:', data)
